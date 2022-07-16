be21b021fca203640c52a1971b5f7d85c2f55f89
Erdi Köse <me@erdkse.com>
2022-07-16 23:24:26 +0300

    chore: rename file names

--- libs/web-components/src/components/dropdown/index.tsx
+++ libs/web-components/src/components/dropdown/index.tsx
@@ -2,7 +2,7 @@ import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'pf-dropdown',
-  styleUrl: 'index.scss',
+  styleUrl: 'style.scss',
   shadow: true,
 })
 export class PfDropdown {
